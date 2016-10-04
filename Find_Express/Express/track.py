9eae109364bf3b64947798d00e81450604c50499
Nrgeup <wangkeanhui@163.com>
2016-10-04 22:16:37 +0800

    finish the recognise function

---
+++ Find_Express/Express/track.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# encoding:utf-8
+
+import recoginse
+
+data = getcompany(expresscode, "1266271", "7526a46e-3a2a-4f5b-8659-d72f361e3386",
+               'http://testapi.kdniao.cc:8081/Ebusiness/EbusinessOrderHandle.aspx')
