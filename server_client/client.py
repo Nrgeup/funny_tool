8675aaa83457bce78aad38d8e33a5ace52f26494
Nrgeup <wangkeanhui@163.com>
2016-10-30 16:20:15 +0800

    add server_client

--- server_client/client.py
+++ server_client/client.py
@@ -1,12 +1,15 @@
 import socket
 import time
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('localhost', 8001))
-time.sleep(2)
-send_content = b'1'
-sock.send(send_content)
-print(sock.recv(1024))
-sock.close()
+while True:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('localhost', 8001))
+    time.sleep(2)
+    send_content = input("Please input :")
+    # str to bytes
+    send_content = str.encode(send_content)
+    sock.send(send_content)
+    print(sock.recv(1024))
+    sock.close()
 
 
